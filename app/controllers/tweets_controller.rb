bf5f8068e3ac06c050191ca92a1c0d5096172680
hyperkumakuma321-tec <marbletyokotec@gmail.com>
2025-08-02 11:14:15 +0900

    記載をまとめる

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,6 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only:[edit,show]
+
   def index
     @tweets = Tweet.all
   end
@@ -19,8 +21,6 @@ class TweetsController < ApplicationController
   end
 
   def edit
-    @tweet = Tweet.find(params[:id])
-
   end
 
   def update
@@ -30,7 +30,6 @@ class TweetsController < ApplicationController
   end
 
   def show
-    @tweet = Tweet.find(params[:id])
   end
 
   private
@@ -38,5 +37,7 @@ class TweetsController < ApplicationController
     params.require(:tweet).permit(:name, :image, :text)
   end
 
-
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
 end
